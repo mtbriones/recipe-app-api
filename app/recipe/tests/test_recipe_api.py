f3cc6d0cf4275e531c47286fccf4967429c15cdd
Michael Briones <mike@pahrsek.com>
2020-08-12 04:19:12 +0800

    Added recipe image upload feature

--- app/recipe/tests/test_recipe_api.py
+++ app/recipe/tests/test_recipe_api.py
@@ -231,8 +231,8 @@ class RecipeImageUploadTests(TestCase):
             img.save(ntf, format='JPEG')
             ntf.seek(0)
 
-            res = self.client.post(url, {'iamge': ntf}, format='mutipart')
-        
+            res = self.client.post(url, {'image': ntf}, format='multipart')
+
         self.recipe.refresh_from_db()
         self.assertEqual(res.status_code, status.HTTP_200_OK)
         self.assertIn('image', res.data)
